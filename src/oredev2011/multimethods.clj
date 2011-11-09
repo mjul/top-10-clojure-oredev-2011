4a74342e0837e95f80035a110d7fef7f637ff0c9
Martin Jul <martin@mjul.com>
2011-11-09 15:52:34 +0100

    Added example data for multimethods.

--- src/oredev2011/multimethods.clj
+++ src/oredev2011/multimethods.clj
@@ -19,23 +19,24 @@
 
 ;; Polymorphism on the value of the :type member
 (defmulti description :type)
-(defmethod description :drink [x]  (str "Drink a wonderful " (:name x)))
+(defmethod description :beverage [x]  (str "Drink a wonderful " (:name x)))
 (defmethod description :food [x]   (str "Savour a tasty " (:name x)))
 
 
 (comment
 
   (def fish-and-chips (LegacyMenuItemClass. "Fish and Chips"))
-  (def espresso (menu-item "Espresso" :drink 12))
-  (def cortado (menu-item "Cortado" :drink 16))
+  (def espresso (menu-item "Espresso" :beverage 12))
+  (def cortado (menu-item "Cortado" :beverage 16))
   (def royale (menu-item "Burger Royale" :food 100))
   (def big-kahuna (menu-item "Big Kahuna Burger" :food 100))
-  
+
+  ;; They are all menu items
   (menu-item? "Hello, World!")
   (menu-item? royale)
   (menu-item? fish-and-chips)
 
+  ;; And the description adapts to the value of :type
   (description big-kahuna)
-  
+ 
 )
-
